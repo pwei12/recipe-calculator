019c5c41747f66a62a3ff7516f4e87c62c916fcc
pi.wei <pi.wei@online-pajak.com>
2020-08-01 01:04:06 +0800

    return cost per kg of raw ingredient in two decimal places

--- src/RecipeCalculator.spec.ts
+++ src/RecipeCalculator.spec.ts
@@ -1,22 +1,35 @@
-import {
-    calculateCostPerUnit,
-    calculateCostPerKg
-} from "./RecipeCalculator";
+import { calculateCostPerUnit, calculateCostPerKg } from "./RecipeCalculator";
+
+const ingredients = [
+	{
+		id: "1",
+		name: "Chocolate 55%",
+		weight: 1000,
+		cost: 10,
+	},
+	{
+		id: "3",
+		name: "Sugar",
+		weight: 1500,
+		cost: 10,
+	},
+];
 
 describe("calculateCostPerUnit", () => {
-    const chocolate = {
-        id: "1",
-        name: "Chocolate 55%",
-        weight: 1000,
-        cost: 10,
-    };
-
 	it("should return cost of raw ingredient as cost per unit", () => {
+		const chocolate = ingredients[0];
 		expect(calculateCostPerUnit(chocolate)).toBe(10);
-    });
-    
-    it('should return cost per kg of raw ingredient', () => {
-		expect(calculateCostPerKg(chocolate.cost, chocolate.weight)).toBe(10);
-    });
+	});
 });
 
+describe("calculateCostPerKg", () => {
+	it("should return cost per kg of raw ingredient", () => {
+		const chocolate = ingredients[0];
+		expect(calculateCostPerKg(chocolate.cost, chocolate.weight)).toBe(10);
+	});
+
+	it("should return cost per kg of raw ingredient in two decimal places", () => {
+		const sugar = ingredients[1];
+		expect(calculateCostPerKg(sugar.cost, sugar.weight)).toBe(6.67);
+	});
+});
